2cf194dddfc4d03ab97b541f99360ebbc527e28f
Maciej Gos <maciej@maciejgos.com>
2020-06-02 16:45:08 +0200

    Change data model

--- src/BackofficeApp/GetHeroRequestsFunction.cs
+++ src/BackofficeApp/GetHeroRequestsFunction.cs
@@ -30,6 +30,9 @@ namespace BackofficeApp
     public class Request
     {
         public string Id { get; set; }
-        public string[] Powers { get; set; }
+        public string Powers { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
     }
 }
