e94ed61f7e287b35b7d5e9c3a0288e37924ec3d2
Maciej Gos <maciej@maciejgos.com>
2020-06-12 07:15:04 +0200

    Some cleanup

--- src/HireHero.FunctionApps/GetHeroRequestsFunction.cs
+++ src/HireHero.FunctionApps/GetHeroRequestsFunction.cs
@@ -1,4 +1,3 @@
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
@@ -7,12 +6,12 @@ using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using System.Collections.Generic;
 
-namespace BackofficeApp
+namespace HireHero.FunctionApp
 {
     public class GetHeroRequestsFunction
     {
         [FunctionName("GetHeroRequests")]
-        public async Task<IActionResult> Run(
+        public IActionResult Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", Route = "offers/requests")] HttpRequest req,
             [CosmosDB(databaseName:"heroapp-db",
             collectionName:"requests",
