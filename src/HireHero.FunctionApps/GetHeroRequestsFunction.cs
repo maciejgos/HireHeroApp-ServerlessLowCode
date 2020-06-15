3202a6ac3e7a96e81db0109b6d572f0fc23d9307
Maciej Gos <maciej@maciejgos.com>
2020-06-15 17:03:57 +0200

    Refactor

--- src/HireHero.FunctionApps/GetHeroRequestsFunction.cs
+++ src/HireHero.FunctionApps/GetHeroRequestsFunction.cs
@@ -25,13 +25,4 @@ namespace HireHero.FunctionApp
             return new OkObjectResult(JsonConvert.SerializeObject(requests));
         }
     }
-
-    public class Request
-    {
-        public string Id { get; set; }
-        public string Powers { get; set; }
-        public string Firstname { get; set; }
-        public string Lastname { get; set; }
-        public string Email { get; set; }
-    }
 }
